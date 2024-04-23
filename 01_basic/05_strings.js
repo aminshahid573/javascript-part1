05a709e8f77295679e77ce6f0bc849a02d72118c
aminshahid573 <createa557733@gmail.com>
2024-04-23 21:13:14 +0545

    string

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,8 @@
+const { log } = require("console");
+
+const name = "shahid";
+const repoCount = 50;
+
+// console.log(name + repoCount + "Value"); //not recommended
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
