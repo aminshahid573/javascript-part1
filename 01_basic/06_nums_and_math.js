660ba4419670dbd0fe494b8c1ce71ac686fac067
aminshahid573 <createa557733@gmail.com>
2024-04-24 07:57:02 +0545

    strings and math updated

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,18 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.5685;
+console.log(otherNumber.toPrecision(3)); // be careful try input 23.5685 and 1123.5685
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-IN')); // indian standard
+
+//+++++++++++ Maths ++++++++++//
+
+
