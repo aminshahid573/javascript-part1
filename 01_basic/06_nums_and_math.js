a7d8779411673eb94cdb6443e4c251b9c4ccc15d
aminshahid573 <createa557733@gmail.com>
2024-04-24 08:05:31 +0545

    updated

--- 01_basic/06_nums_and_math.js
+++ 01_basic/06_nums_and_math.js
@@ -11,8 +11,17 @@ const otherNumber = 123.5685;
 console.log(otherNumber.toPrecision(3)); // be careful try input 23.5685 and 1123.5685
 
 const hundreds = 1000000;
-console.log(hundreds.toLocaleString('en-IN')); // indian standard
+console.log(hundreds.toLocaleString("en-IN")); // indian standard
 
 //+++++++++++ Maths ++++++++++//
 
+console.log(Math); //try writing in chrome console
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2)); // if decimal value is just greater than 0 i.e 4.1 , 4.2 it rond of to 5
+console.log(Math.floor(4.2));
+console.log(Math.min(4, 3, 2, 1));
+console.log(Math.max(4, 3, 2, 1));
 
+console.log(Math.random()); // between 0 and 1
+console.log((Math.random()*10) +1 );
